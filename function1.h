882a15af1461e389b0a2f468451c70f105cdfca5
omar-moustafa-code <omoustafa@aucegypt.edu>
2023-06-11 17:45:10 +0300

    Added function1

---
+++ function1.h
@@ -0,0 +1,10 @@
+//
+// Created by Omar Moustafa on 11/06/2023.
+//
+
+#ifndef CSCE1101_LAB1_FUNCTION1_H
+#define CSCE1101_LAB1_FUNCTION1_H
+
+int knapSack(int W, int wt[], int val[], int n);
+
+#endif //CSCE1101_LAB1_FUNCTION1_H
